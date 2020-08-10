e6db33573d731a470538c937ab480eae9737e4b9
lmachens <leon.machens@googlemail.com>
2020-08-10 14:07:24 +0200

    Show search results with link

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,13 @@
 import { createElement } from "../utils/elements";
 
 function Results(props) {
-  const listItems = [];
-  props.values.forEach((value) => {
-    listItems.push(createElement("li", { innerText: value }));
+  const listItems = props.issues.map((issue) => {
+    const link = createElement("a", {
+      href: issue.html_url,
+      innerText: issue.title,
+      target: "_blank",
+    });
+    return createElement("li", {}, [link]);
   });
 
   const list = createElement("ul", {}, listItems);
