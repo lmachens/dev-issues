c69b3792ee93f3aba1b044e024d4473780a5d00d
lmachens <leon.machens@googlemail.com>
2020-08-10 09:55:30 +0200

    Import app.css

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,12 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
