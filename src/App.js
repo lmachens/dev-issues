a05985a9f07c5258aa5610791c91ba24e2ecad12
lmachens <leon.machens@googlemail.com>
2020-08-10 13:49:29 +0200

    Use search input in API call

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   async function handleChange(query) {
     console.log(query);
-    const issues = await getIssues();
+    const issues = await getIssues(query);
     console.log(issues);
 
     const newResults = Results({
--- src/api/gitHub.js
+++ src/api/gitHub.js
@@ -1,5 +1,12 @@
-export async function getIssues() {
-  const response = await fetch("https://api.github.com/search/issues?q=test");
+function createIssuesURL(query) {
+  return `https://api.github.com/search/issues?q=${query}`;
+  // return "https://api.github.com/search/issues?q=".concat(query);
+  // return "https://api.github.com/search/issues?q=" + query;
+}
+
+export async function getIssues(query) {
+  const url = createIssuesURL(query);
+  const response = await fetch(url);
   const result = await response.json();
   return result;
 }
